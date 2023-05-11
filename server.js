e5159bf3d34f609973862bf5cec8c704e031b856
mikaelmonteirodev <mikaelmonteiro@gmail.com>
2023-05-11 20:11:14 -0300

    feat: add fs dirname

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
+import fs  from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-app.use(cors()  )
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// const express = require('express')
+
 const app = express()
 
+app.use(cors())
+// fetch('http://localhost?abraao')
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World: ' )
 })
 
-app.listen(3000)
+const filename = __dirname + '/index.html';
+app.get('/toligado', function (req, res) {
+
+const fileContents = fs.readFileSync(filename).toString();
+
+console.log(fileContents);
+  res.send(fileContents )
+})
+
+const PORT = 9000;
+
+app.listen(PORT, () => {
+  console.log('Servidor pronto p execução na porta: ', PORT);
+  console.log('Você pode fazer requests agora da seguinte forma: ');
+  console.log(`GET fetch("http://localhost:${PORT}?abraao") `);
+
+})
